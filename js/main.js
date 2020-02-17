741dd42efd0f081fc26d241e65b406d0a0db8f23
wing MBP <eamonn.kearns@so-4pt.net>
2020-02-17 15:33:23 -0800

    Just doing some simple cleanup.
    It occurs to me I might reuse some of this code, so I'd like it more presentably.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 (function($){
   let ctx;
   let canvas;
-  let imageLoader = new Image();
+  let blankImageHTMLEntity = new Image();
   let visibleImage;
+
+  let maxWidth;
+  let maxHeight;
 
   let $bad;
   let $good;
@@ -29,98 +32,8 @@
     ctx.font = `${currentFontHeight}px ${font}`;
   }
 
-  function wrapMemeText(text, left, top)
+  function renderTextLines(lines, top, left)
   {
-    currentFontHeight = defaultFontHeight;
-    linePadding = defaultLinePadding;
-    ctx.font = `${currentFontHeight}px ${font}`;
-    if(!text.length)
-    {
-      return;
-    }
-    let words = text.split(' ');
-
-
-    // 5 padding on either side
-    let maxWidth = canvas.width / 2 - 10;
-    let maxHeight = canvas.height / 2 - 10;
-    // ticker for getting an individual word
-    let wc = 0;
-    // ticker for the line count
-    let lc = 0;
-
-    // first ensure that no single word is longer than the width
-    while(wc <= words.length)
-    {
-      let word = words[wc];
-      let wordWidth = ctx.measureText(word).width;
-      while(wordWidth > maxWidth)
-      {
-        reduceCurrentFontHeight();
-        wordWidth = ctx.measureText(word).width;
-      }
-      wc++;
-    }
-
-    // an array to hold all lines
-    let lines = [];
-
-    let shrinking = true;
-    while(shrinking)
-    {
-      // assume that the font shrinking worked
-      shrinking = false;
-      // reset the ticker
-      wc = 0;
-      // now add a word a time to the current line
-      let addingWords = true;
-      // a flag to check whether or not to add a preceding space to the current word
-      // which we only do if it's not the first word in the sentence
-      let firstWord = true;
-      // a string to hold the current line that is being concatenated
-      let currentLine = '';
-      // the text bottom for the first line
-      let textBottom = top + currentFontHeight + 5;
-
-      for(let word of words)
-      {
-        // add a space to the word if it's not the first word in the line
-        if(!firstWord)
-        {
-          word = ` ${word}`;
-        }
-
-        // get the current line width
-        let currentLineWidth = ctx.measureText(currentLine).width;
-        // and the current word width
-        let currentWordWidth = ctx.measureText(word).width;
-        // check if we can add this line without causing wrap
-        if((currentWordWidth + currentLineWidth) > maxWidth)
-        {
-          // add the line to the array and clear it.
-          lines.push(currentLine);
-          currentLine = word.substr(1);
-        }
-        else
-        {
-          // add the word to the current line
-          currentLine += word;
-          firstWord = false;
-        }
-      }
-      lines.push(currentLine);
-
-      // check that the current array fits within the max height
-      let totalHeight = lines.length * (currentFontHeight + linePadding);
-      shrinking = totalHeight > maxHeight;
-      if(shrinking)
-      {
-        reduceCurrentFontHeight();
-        lines = [];
-        currentLine = '';
-      }
-    }
-
     let lineBottom = top + currentFontHeight;
     for(let line of lines)
     {
@@ -142,16 +55,121 @@
     }
   }
 
+  function reduceFontForLargestWord(words)
+  {
+    let wc = 0;
+    // first ensure that no single word is longer than the width
+    while(wc <= words.length)
+    {
+      let word = words[wc];
+      let wordWidth = ctx.measureText(word).width;
+      while(wordWidth > maxWidth)
+      {
+        reduceCurrentFontHeight();
+        wordWidth = ctx.measureText(word).width;
+      }
+      wc++;
+    }
+  }
+
+  /**
+   * Make a set of lines from words, making sure that the lines all fit width wise.
+   * This may result in words going over the maximum height of the assigned area
+   * @param words
+   * @returns {[]}
+   */
+  function generateLineArrayFromWordArray(words)
+  {
+    let lines = [];
+    let currentLine = '';
+    let firstWord = true;
+    for(let word of words)
+    {
+      // add a space to the word if it's not the first word in the line
+      if(!firstWord)
+      {
+        word = ` ${word}`;
+      }
+
+      // get the current line width
+      let currentLineWidth = ctx.measureText(currentLine).width;
+      // and the current word width
+      let currentWordWidth = ctx.measureText(word).width;
+      // check if we can add this line without causing wrap
+      if((currentWordWidth + currentLineWidth) > maxWidth)
+      {
+        // add the line to the array and clear it.
+        lines.push(currentLine);
+        currentLine = word.substr(1);
+      }
+      else
+      {
+        // add the word to the current line
+        currentLine += word;
+        firstWord = false;
+      }
+    }
+    lines.push(currentLine);
+    return lines;
+  }
+
+  function addWrappedTextToCanvas(text, left, top)
+  {
+    if(!text.length)
+    {
+      return;
+    }
+    // default to whatever constant I use for the font height default
+    currentFontHeight = defaultFontHeight;
+    // do the same for padding
+    linePadding = defaultLinePadding;
+    // default the font so we can measure the text appropriately
+    ctx.font = `${currentFontHeight}px ${font}`;
+
+    let words = text.split(' ');
+
+    // make sure that the largest single word can definitely fit on the screen
+    reduceFontForLargestWord(words);
+
+    // an array to hold all lines
+    let lines;
+
+    let shrinking = true;
+    while(shrinking)
+    {
+      // assume that the font shrinking worked
+      shrinking = false;
+
+      lines = generateLineArrayFromWordArray(words);
+
+      // check that the current array fits within the max height and if not, shrink the text
+      // and start the entire process again
+      let totalHeight = lines.length * (currentFontHeight + linePadding);
+      shrinking = totalHeight > maxHeight;
+      if(shrinking)
+      {
+        reduceCurrentFontHeight();
+      }
+    }
+
+    renderTextLines(lines,  top, left);
+  }
+
   function generateMeme()
   {
+    // empty the canvas drawing area
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(imageLoader, 0, 0);
+    // draw a blank geordi meme onto the canvas
+    ctx.drawImage(blankImageHTMLEntity, 0, 0);
 
+    // get the text of the good input
     let good = $good.val();
+    // get the text of the bad input
     let bad = $bad.val();
 
-    wrapMemeText(bad, 365, 5);
-    wrapMemeText(good, 365, 365);
+    // wrap both of those texts and render them to the canvas
+    addWrappedTextToCanvas(bad, 365, 5);
+    addWrappedTextToCanvas(good, 365, 365);
 
     visibleImage.src = canvas.toDataURL("image/png");
 
@@ -159,6 +177,7 @@
 
   function makeItSo()
   {
+    // create an image and make it the source of a link for convenient downloading
     let image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
     let a = this;
     a.href = image;
@@ -172,34 +191,27 @@
   }
 
   $(function(){
+    // the image we'll actually be drawing to
+    visibleImage = document.getElementById('gmg_visible');
+
+    // canvas stuff
     canvas = document.getElementById('gmg_hidden');
     canvas.width = canvas.height = 720;
-
     ctx = canvas.getContext('2d');
     ctx.font = `${currentFontHeight}px ${font}`;
 
-    imageLoader.onload = generateMeme;
-    imageLoader.src = './img/blank.png';
+    // 5 padding on either side
+    maxWidth = canvas.width / 2 - 10;
+    maxHeight = canvas.height / 2 - 10;
 
-    visibleImage = document.getElementById('gmg_visible');
+    // set a loader for the canvas
+    blankImageHTMLEntity.onload = generateMeme;
+    blankImageHTMLEntity.src = './img/blank.png';
 
-    let urlVars = window.location.href.split('?')[1];
-    let parts = {bad:'', good:''};
-    if(urlVars)
-    {
-      let urlPairs = urlVars.split('&');
-      for(let pair of urlPairs)
-      {
-        let [key, val] = pair.split('=');
-        val = decodeURI(val);
-        parts[key.toLowerCase()] = val;
-      }
-    }
-
-    $bad = $('#meme-bad').val(parts.bad).change(generateMeme);
-    $good = $('#meme-good').val(parts.good).change(generateMeme);
+    // bind the event handlers
+    $bad = $('#meme-bad').change(generateMeme);
+    $good = $('#meme-good').change(generateMeme);
     $('#save-meme').click(makeItSo);
-
     $('.alignment-btn').click(setAlignment);
 
   });
